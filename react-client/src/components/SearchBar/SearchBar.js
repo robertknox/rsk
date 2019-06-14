48a284672ad5b711ec43b7e236511a9292e494a1
Robert Knox <robertselkirkknox@gmail.com>
2019-06-13 17:39:06 -0700

    Initial commit.

---
+++ react-client/src/components/SearchBar/SearchBar.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import './SearchBar.css';
+
+
+
+const sortByOptions = {
+'Best Match': 'best_match',
+  'Highest Rated': 'rating',
+  'Most Reviewed': 'review_count'
+}
+
+
+class SearchBar extends React.Component {
+
+
+	constructor(props){
+		super(props);
+		this.state = {
+			term: '',
+			location: '',
+			sortBy: 'best_match' 
+		}
+
+		this.handleLocationChange.bind(this);
+		this.handleTermChange.bind(this);
+		this.handleSearchYelp.bind(this)
+	}
+
+
+	handleSearchYelp(event){
+	   this.props.searchYelp(this.state.term,this.state.location,this.state.sortBy);
+	   event.preventDefault()
+	   
+	}
+
+	getSortByClass(sortByOption){
+ 		if (sortByOption === this.state.sortBy){
+			console.log("getSortByClass:" + sortByOption + " is active");
+			return 'active';
+		}
+		else return ''
+	}
+
+	handleSortByChange( sortByVal){
+		this.setState({sortBy: sortByVal });
+	}
+
+	handleTermChange( event ){
+		this.setState({term: event.target.value });
+	}
+
+
+	handleLocationChange( event ){
+		this.setState({location: event.target.value });
+	}
+
+	renderSortByOptions(){
+
+	return (
+		Object.keys(sortByOptions).map(sortByOption => {
+			let sortByOptionValue = sortByOptions[sortByOption];
+			return <li key={sortByOptionValue} onClick={this.handleSortByChange.bind(this, sortByOptionValue)} className={this.getSortByClass(sortByOptionValue)}>{sortByOption}</li>;
+		})
+	)
+	}
+
+	render(){
+	return (
+<div className="SearchBar">
+  <div className="SearchBar-sort-options">
+    <ul>
+      {this.renderSortByOptions()} 
+    </ul>
+  </div>
+  <div className="SearchBar-fields">
+    <input onClick={this.handleTermChange} placeholder="Search Businesses" />
+    <input onClick={this.handleLocationChange} placeholder="Where?" />
+  </div>
+  <div  className="SearchBar-submit">
+    <a onClick={this.handleSearchYelp} >Let's Go</a>
+  </div>
+</div>
+
+	)
+	}
+}
+
+export default SearchBar
