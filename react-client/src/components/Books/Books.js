23a47c064391bcd4c830df177444c921a937a81d
robertknox <robertselkirkknox@yahoo.com>
2019-06-20 09:31:13 -0700

    added Books

---
+++ react-client/src/components/Books/Books.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './SearchBar.css';
+
+
+class Books  extends React.Component {
+
+
+	constructor(props){
+	}
+
+
+
+	render(){
+		return (
+			<div className="Books">
+			</div>
+
+		)
+	}
+}
+
+export default Books 
