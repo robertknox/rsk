30c6e27ecca1016202c8f46b42891e93e6b7a119
robertknox <robertselkirkknox@yahoo.com>
2019-06-24 16:06:28 -0700

    added AboutAuthor

---
+++ react-client/src/components/AboutAuthor/AboutAuthor.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './SearchBar.css';
+
+
+class AboutAuthor  extends React.Component {
+
+
+	constructor(props){
+	}
+
+
+
+	render(){
+		return (
+			<div className="AboutAuthor">
+			</div>
+
+		)
+	}
+}
+
+export default AboutAuthor 
