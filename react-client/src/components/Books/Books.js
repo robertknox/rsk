c31fff7397a5793ae990bbc166fb2e6bca29c68a
robertknox <robertselkirkknox@yahoo.com>
2019-07-08 13:49:40 -0700

    Books add image

--- react-client/src/components/Books/Books.js
+++ react-client/src/components/Books/Books.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './Books.css';
+import './BBBCover.png';
+import './BBB.jpg';
 
 
 export class Books  extends React.Component {
 
 
+
 	render(){
+
+		let art = [ 	 'jumper',  'red', 'medium', ];
+		let  test = art[1];
+
 		return (
 			<div className="Books">
 				Books.
 				Barcelona, The Beautiful Bones can be puchased at amazon.com in both e-book and hardcopy.
 				San Francisco, Sirens In The Ferns, will be released shortly.
+				{art.map((item) => (
+					<div className="ABook">
+					<h1>{item} </h1>
+					<img src='./BBB.png' />
+					</div>
+					))} 
 			</div>
 
 		)
