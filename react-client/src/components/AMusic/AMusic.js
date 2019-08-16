4f0c734de853d106ea5d5dd049ebecf8d106f53b
robertknox <robertselkirkknox@yahoo.com>
2019-08-15 17:15:39 -0700

    added AMusic

---
+++ react-client/src/components/AMusic/AMusic.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+export class AMusic extends React.Component{
+	render(){
+		return (
+			<div >
+				TEST AMUSIC
+			</div>
+		)
+	}
+}
+
+
+export default AMusic;
+
+
--- react-client/src/components/Music/Music.js
+++ react-client/src/components/Music/Music.js
@@ -7,11 +7,18 @@ export class Music extends React.Component {
 
 	   let music_array = [{title: "Barraca Leblon"},
 		{title: "Reaching Back to ScottLand"},
-		{title: "Rock and Roll -- A long time ago."}
+		{title: "Rock And Roll -- A roll and a rock."}, 
 	   ];
+
 	   return (
 		<div className="Music">
 		       <h1>Music CDs Available</h1>
+			{music_array.map((item) => (
+				<div class="A_CD">			
+				<h2>{ item.title} </h2>
+				<img src={require("./BBBCover.png")}/>
+				</div>
+			))}
                 </div>
 	    );
 	}
